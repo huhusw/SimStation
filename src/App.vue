b7fd0db8af6cf8827dd99166634cec0b128fae90
huhusw <1241671281@qq.com>
2020-03-06 17:49:01 +0800

    vue-particles插件的使用

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <template>
   <div id="app">
+    <vue-particles
+      color="#dedede"
+        :particleOpacity="0.7"
+        :particlesNumber="80"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#dedede"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push">
+        </vue-particles>
     <div id="nav">
+      
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+
     <router-view/>
+    
   </div>
 </template>
+<script type="text/javascript" src="https://cdn.bootcss.com/canvas-nest.js/1.0.1/canvas-nest.min.js"></script>
+<script>
+  export default{
+    name: 'App',
+    components:{
+    }
+  }
+</script>
 
 <style>
 #app {
